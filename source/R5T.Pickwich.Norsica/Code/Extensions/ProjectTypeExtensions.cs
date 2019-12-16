6cd486f6c137c40b3dee766f5ec927e33fd16c09
MinexAutomation <minex.automation@gmail.com>
2019-12-16 14:23:53 -0800

    Implemented the visual studio project file operator. Added project-type extensions to get the corresponding dotnet project template short-name.

---
+++ source/R5T.Pickwich.Norsica/Code/Extensions/ProjectTypeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using R5T.Magyar;
+using R5T.Pickwich.Types;
+
+
+namespace R5T.Pickwich.Norsica
+{
+    public static class ProjectTypeExtensions
+    {
+        public static string ToProjectTemplateShortName(this ProjectType projectType)
+        {
+            switch(projectType)
+            {
+                case ProjectType.Console:
+                    return "console";
+
+                case ProjectType.Library:
+                    return "classlib";
+
+                case ProjectType.Test:
+                    return "mstest";
+
+                default:
+                    throw new Exception(EnumHelper.UnexpectedEnumerationValueMessage(projectType));
+            }
+        }
+    }
+}
---
+++ source/R5T.Pickwich.Norsica/Code/Services/Implementations/VisualStudioProjectFileOperator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using R5T.Norsica;
+using R5T.Pickwich.Types;
+
+
+namespace R5T.Pickwich.Norsica
+{
+    public class VisualStudioProjectFileOperator : IVisualStudioProjectFileOperator
+    {
+        private IDotnetOperator DotnetOperator { get; }
+        private IVisualStudioProjectFilePathProvider VisualStudioProjectFilePathProvider { get; }
+
+
+        public VisualStudioProjectFileOperator(IDotnetOperator dotnetOperator, IVisualStudioProjectFilePathProvider visualStudioProjectFilePathProvider)
+        {
+            this.DotnetOperator = dotnetOperator;
+            this.VisualStudioProjectFilePathProvider = visualStudioProjectFilePathProvider;
+        }
+
+        public string CreateProjectFile(ProjectType projectType, string projectDirectoryPath, string projectName)
+        {
+            var projectTemplateShortName = projectType.ToProjectTemplateShortName();
+
+            this.DotnetOperator.CreateNewProjectFile(projectTemplateShortName, projectDirectoryPath, projectName);
+
+            var projectFilePath = this.VisualStudioProjectFilePathProvider.GetVisualStudioProjectFilePath(projectDirectoryPath, projectName);
+            return projectFilePath;
+        }
+    }
+}
